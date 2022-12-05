516c3bf30244d2787fd78826bb85ecd06ef5d05a
purpletina <113041666+purpletina@users.noreply.github.com>
2022-12-06 00:47:04 +0800

    Update Class17.java

--- Class17.java
+++ Class17.java
@@ -1,1 +1,9 @@
-
+package Java12;
+public class class17 {
+	public static void main(String[] args) {
+		String str="262904713";
+		short num;
+		num = Short.parseShort(str);
+		System.out.println(num*2);
+	}
+}
