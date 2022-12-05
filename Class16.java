adad65f5c602f8be3e3ccb12c56e44de0db5133d
purpletina <113041666+purpletina@users.noreply.github.com>
2022-12-06 00:43:50 +0800

    Update Class16.java

--- Class16.java
+++ Class16.java
@@ -1,1 +1,9 @@
-
+package Java12;
+public class class16 {
+	public static void main(String[] args) {
+		String str="262904713";
+		int num;
+		num = Integer.parseInt(str);
+		System.out.println(num*2);
+	}
+}
