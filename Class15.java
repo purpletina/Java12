d2522a022d96358870819a0a1b9e1b346e77c2ad
purpletina <113041666+purpletina@users.noreply.github.com>
2022-12-06 00:39:20 +0800

    Update Class15.java

--- Class15.java
+++ Class15.java
@@ -1,1 +1,9 @@
-
+package Java12;
+public class class15 {
+	public static void main(String[] args) {
+		String str;
+		str=Float.toString(123.45f);
+		System.out.println(str);
+		System.out.println("length="+str.length());
+	}
+}
